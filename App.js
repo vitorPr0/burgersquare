9ad2cb490a6a8255e276da9ee9e73332b97d93e6
IsabelaSabino <isabela.sabinosenai@gmail.com>
2024-04-11 11:24:02 -0300

    Ajustes

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import  MaterialCommunityIcons  from 'react-native-vector-icons/MaterialCommunityIcons';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 import { useState } from 'react';
 import Home from './src/Home'
 import Carrinho from './src/Carrinho'
@@ -19,62 +19,62 @@ import Mapa from './src/Mapa';
 const Tab = createBottomTabNavigator()
 export default function App() {
 
- /* const[ logado, setLogado] = useState(false);
-  const[cadastro, setCadastro] = useState(false);
+  const [logado, setLogado] = useState(false);
+  const [cadastro, setCadastro] = useState(false);
 
 
-    if(!logado) {
-      return(<Login setLogado={setLogado} setCadastro={setCadastro}/> )
-    }
+  if (!logado) {
+    return (<Login setLogado={setLogado} setCadastro={setCadastro} />)
+  }
 
-    if(cadastro){
-      return( <Cadastro setCadastro={setCadastro} setLogado={setLogado}/> )
-    }*/
+  if (cadastro) {
+    return (<Cadastro setCadastro={setCadastro} setLogado={setLogado} />)
+  }
 
 
   return (
     <NavigationContainer>
-      <Tab.Navigator  initialRouteName="Home"
-         screenOptions={{
-          headerStyle: {backgroundColor: '#7A361F'},
-          tabBarStyle: {backgroundColor: '#7A361F'},
+      <Tab.Navigator initialRouteName="Home"
+        screenOptions={{
+          headerStyle: { backgroundColor: '#7A361F' },
+          tabBarStyle: { backgroundColor: '#7A361F' },
           tabBarActiveTintColor: "#FFBB5C",
-          tabBarInactiveTintColor:"white"
+          tabBarInactiveTintColor: "white"
 
-         }}
+        }}
       >
         <Tab.Screen name="Home" component={Home}
-         options={{
+          options={{
             tabBarLabel: 'Home',
-            tabBarLabelStyle: {fontSize: 15},
+            tabBarLabelStyle: { fontSize: 15 },
             tabBarIcon: ({ color, size }) => (
-            <MaterialCommunityIcons name="home" color={color} size={size} />
-          ),
-        }}
+              <MaterialCommunityIcons name="home" color={color} size={size} />
+            ),
+          }}
         />
 
         <Tab.Screen name="Pedidos" component={Pedidos}
           options={{
             tabBarLabel: 'Pedidos',
-            tabBarLabelStyle: {fontSize: 15},
+            tabBarLabelStyle: { fontSize: 15 },
             tabBarIcon: ({ color, size }) => (
-            <MaterialCommunityIcons name="food" color={color} size={size} />
+              <MaterialCommunityIcons name="food" color={color} size={size} />
             ),
           }}
         />
 
         <Tab.Screen name="Perfil" component={Perfil}
-           options={{
+          options={{
             tabBarLabel: 'Perfil',
-            tabBarLabelStyle: {fontSize: 15},
+            tabBarLabelStyle: { fontSize: 15 },
             tabBarIcon: ({ color, size }) => (
-            <MaterialCommunityIcons name="account" color={color} size={size} />
+              <MaterialCommunityIcons name="account" color={color} size={size} />
             ),
           }}
-          
+
         />
-        </Tab.Navigator>
-        </NavigationContainer>
+      </Tab.Navigator>
+    </NavigationContainer>
 
   );
 }
